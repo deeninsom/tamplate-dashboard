dc1ef0d06a27e87419b5658626414b1db27c1a26
deeninsom <syihab855@gmail.com>
2024-03-22 19:34:37 +0700

    fix: update responsive

--- src/component/Navigaton/Sidebar/index.tsx
+++ src/component/Navigaton/Sidebar/index.tsx
@@ -3,15 +3,21 @@ import { useState } from 'react'
 import logo from '../../../../src/assets/image/logo-white.png'
 import { listMenuSidebar } from "../../../dumy-data/UlSidebar"
 
-const Sidebar = () => {
+interface Props {
+    openSide: (value: boolean) => void;
+    setOpenSide: (value: boolean) => void;
+}
+
+const Sidebar = (props: Props) => {
     const [isMenuActive, setIsMenuActive] = useState('Dashboard')
+    const { openSide } = props
 
     return (
-        <div className="w-72 h-[100vh] hidden lg:block dark:bg-gray-900">
+        <div className={!openSide ? `w-72 h-[100vh] hidden lg:block dark:bg-gray-900` : `w-48 h-[100vh] z-[90] absolute lg:block dark:bg-gray-900`}>
             <div className="px-6 mt-5">
                 <div className="flex-none text-xl font-semibold dark:text-white flex items-center justify-items-center gap-2" aria-label="Brand">
                     <img src={logo} width={30} />
-                    <span className='text-[17px]'>THEFAVORED-ONE</span>
+                    <span className={!openSide ? `text-[17px]` : `text-[11px]`}>THEFAVORED-ONE</span>
                 </div>
             </div>
 
@@ -54,25 +60,25 @@ const Sidebar = () => {
                                                                     <svg className="hs-accordion-active:block ms-auto hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6" /></svg>
                                                                     <svg className="hs-accordion-active:hidden ms-auto block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6" /></svg>
                                                                 </button>
-                                                                {/* <div id="users-accordion-sub-1-child" className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden">
-                                                            <ul className="pt-2 ps-2">
-                                                                <li>
-                                                                    <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-lg hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-600" href="#">
-                                                                        Link 1
-                                                                    </a>
-                                                                </li>
-                                                                <li>
-                                                                    <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-lg hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-600" href="#">
-                                                                        Link 2
-                                                                    </a>
-                                                                </li>
-                                                                <li>
-                                                                    <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-lg hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-600" href="#">
-                                                                        Link 3
-                                                                    </a>
-                                                                </li>
-                                                            </ul>
-                                                        </div> */}
+                                                                <div id="users-accordion-sub-1-child" className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden">
+                                                                    <ul className="pt-2 ps-2">
+                                                                        <li>
+                                                                            <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-lg hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-600" href="#">
+                                                                                Link 1
+                                                                            </a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-lg hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-600" href="#">
+                                                                                Link 2
+                                                                            </a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-lg hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-gray-600" href="#">
+                                                                                Link 3
+                                                                            </a>
+                                                                        </li>
+                                                                    </ul>
+                                                                </div>
                                                             </li>
                                                         </ul>
                                                     </div>
--- src/component/Navigaton/index.tsx
+++ src/component/Navigaton/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
@@ -7,13 +8,14 @@ interface Props {
 }
 
 const Navigation = (props: Props) => {
-
+    const [openSide, setOpenSide] = useState(false)
+    
     return (
         <div className="flex flex-row">
-            <Sidebar />
+            <Sidebar openSide={openSide} setOpenSide={setOpenSide} />
             <div className="w-full">
                 <div className="flex flex-col">
-                    <Topbar />
+                    <Topbar openSide={openSide} setOpenSide={setOpenSide} />
                     <div className="p-4">
                         {props.children}
                     </div>
